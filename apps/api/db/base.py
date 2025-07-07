8f479e0302767a8cdddb844863d5cab07e73c5ea
Abraham12611 <abraham.dahunsi@gmail.com>
2025-07-07 03:25:10 +0100

    Auto-commit: update 'apps/api/db/base.py' at 2025-07-07 03:25:10

--- apps/api/db/base.py
+++ apps/api/db/base.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from core.settings import settings
 
-DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@postgres:5432/{settings.POSTGRES_DB}"
+DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
+from db.models import *
